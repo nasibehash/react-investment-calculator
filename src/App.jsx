1ae4ca534c219eba9c51cb96e3ebb347ad98b26c
Nasibeh Ashtari <n.ashtari@mydigipay.com>
2024-07-01 16:48:14 +0330

    Feat: Add UserInput and Result and Header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
-function App() {
+import Header from './components/Header.jsx';
+import UserInput from './components/UserInput.jsx';
+import Result from './components/Result.jsx';
+import { useState } from 'react';
+
+function App () {
+  const [ userInput, setUserInput ] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  });
+
+  const inputValid = userInput.duration >= 1;
+
+  function handleChange ( inputIdentifier, newValue ) {
+    setUserInput(prevUserInput => {
+      return {
+        ...prevUserInput,
+        [ inputIdentifier ]: +newValue
+      };
+    });
+  }
+
   return (
-    <h1>React Investment Calculator</h1>
-  )
+    <>
+      <Header/>
+      <UserInput userInput={userInput} onChange={handleChange}/>
+      {!inputValid && <p className="center">please enter a duration greater than zero </p>}
+      {inputValid && <Result input={userInput}/>}
+    </>
+  );
 }
 
-export default App
+export default App;
